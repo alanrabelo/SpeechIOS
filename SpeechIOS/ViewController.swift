7c0c193f767b386a01c6bac36e62859641ccdfcb
Cibele Paulino Andrade <cibelepandrade@gmail.com>
2019-09-06 19:24:53 -0300

    convertendo audio curto para texto

--- SpeechIOS/ViewController.swift
+++ SpeechIOS/ViewController.swift
@@ -7,14 +7,46 @@
 //
 
 import UIKit
+import Speech
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        requestPermissionsForSpeech()
     }
 
-
+    func requestPermissionsForSpeech() {
+        SFSpeechRecognizer.requestAuthorization { (status) in
+            DispatchQueue.main.async {
+                if status == .authorized {
+                    print("Autorized")
+                    var url = Bundle.main.url(forResource: "teste", withExtension: "mp3")
+//                    var url = URL.init(fileURLWithPath: "us000121") as? URL
+                    guard let url2 = url else { return }
+                    self.transcribeAudio(url: url2)
+                } else {
+                    print("Not autorized")
+                }
+            }
+        }
+    }
+    
+    func transcribeAudio(url: URL) {
+        let recognize = SFSpeechRecognizer()
+        let request =  SFSpeechURLRecognitionRequest(url: url)
+        
+        recognize?.recognitionTask(with: request, resultHandler: { (result, error) in
+            guard let result = result else {
+                print("No this time")
+                return }
+            if result.isFinal {
+                
+                print(result.bestTranscription.formattedString)
+            }
+        })
+    }
 }
 
