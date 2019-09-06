e9adbfef73436b9d8b03e76925f233f83e65b6d7
Alan Paulino <alanvictor5447@gmail.com>
2019-09-06 16:42:32 -0300

    Initial Commit

---
+++ SpeechIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpeechIOS
+//
+//  Created by Alan Victor Paulino de Oliveira on 06/09/19.
+//  Copyright © 2019 Alan Victor Paulino de Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
