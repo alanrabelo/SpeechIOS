4d2f0ce5273a22d80746c19cada6b24cbe88c1bf
jonhpol <j.paulo_os@hotmail.com>
2019-09-12 14:50:57 -0300

    Text to Speech implementation

---
+++ SpeechIOS/JP/TextToSpeechViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TextToSpeechViewController.swift
+//  SpeechIOS
+//
+//  Created by João Paulo de Oliveira Sabino on 12/09/19.
+//  Copyright © 2019 Alan Victor Paulino de Oliveira. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class TextToSpeechViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var speechButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    @IBAction func speechAction(_ sender: Any) {
+        let speechSynthesizer = AVSpeechSynthesizer()
+        
+        guard let text = textField.text else { return }
+        let speechUtterance: AVSpeechUtterance = AVSpeechUtterance(string: text)
+        speechUtterance.voice = AVSpeechSynthesisVoice(language: "pt-PT")
+        
+        speechSynthesizer.speak(speechUtterance)
+    }
+}
